cc14b34814449d16afbc7fed01ef2bacdadfa9e9
BerkaySzn <brkysezan@gmail.com>
2024-07-22 12:25:44 +0300

    güncel proje

---
+++ Scenes/menu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://Scenes/world.tscn")
+
+
+func _on_settings_pressed():
+	get_tree().change_scene_to_file("res://Scenes/options_menu.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
---
+++ killzone.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+
+
+
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		body.health -= 1
+		print("Player health: ", body.health)
+		
+
+
+
