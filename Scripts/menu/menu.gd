e0d204c0894a966151a8abfdfee041b8ad2ef2b6
berke onver <149507129+berkenvr@users.noreply.github.com>
2024-07-23 12:37:32 +0300

    Quick Script Fix

--- Scripts/menu/menu.gd
+++ Scripts/menu/menu.gd
@@ -4,11 +4,11 @@ extends Control
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://Scenes/world.tscn")
+	get_tree().change_scene_to_file("res://Scenes/world/world.tscn")
 
 
 func _on_settings_pressed():
-	get_tree().change_scene_to_file("res://Scenes/options_menu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/menu/options_menu.tscn")
 
 
 func _on_quit_pressed():
--- Scripts/menu/options_menu.gd
+++ Scripts/menu/options_menu.gd
@@ -8,4 +8,4 @@ func _on_volume_pressed():
 
 
 func _on_back_pressed():
-	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/menu/menu.tscn")
