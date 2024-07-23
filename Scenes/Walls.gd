9367982417086eb92060841dbeb351e1d9d02154
berke onver <149507129+berkenvr@users.noreply.github.com>
2024-07-23 12:28:14 +0300

    fix

---
+++ Scenes/Walls.gd
@@ -0,0 +1,13 @@
+extends Node
+
+@export var pipe_scene: PackedScene # Drag and drop your pipe scene here
+
+func _ready():
+	$SpawnTimer.start()
+
+func _on_spawn_timer_timeout():
+	print("time is up") # Testing
+	var wall = pipe_scene.instantiate()
+	wall.position = Vector2(1024, 624) # The new wall will spawn at that point
+	add_child(wall)
+
